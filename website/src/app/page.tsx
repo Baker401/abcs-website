bbfc427dc4a99d33b061f6d19f32a1c42799ae9a
adbaker1479 <adbaker1479@gmail.com>
2025-01-23 15:04:29 -0500

    init commit with starting navbar

---
+++ website/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/components/header";
+
+export default function Home() {
+  return (
+      <>
+      <Header/>
+    <div>
+      Welcome to A. B. Computer Services!
+    </div>
+      </>
+  );
+}
