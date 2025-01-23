e23715d1a960ccb5d11e6515901ba346c88c1aea
adbaker1479 <adbaker1479@gmail.com>
2025-01-23 18:24:10 -0500

    commit updated image function UC

--- website/src/app/components/image.tsx
+++ website/src/app/components/image.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Image from "next/image";
+
+
 function UnderConstruction() {
-    return <img srcSet={'./assets/under.jpeg'} width='auto' height='auto' alt="Image" />;
+    //return <img srcSet={'./assets/under.jpeg'} className="mx-auto width-500px" alt="Image" />;
+    return <Image
+        src="/assets/under.jpeg"
+        alt="Under Construction"
+        width={2080}
+        height={1080}
+
+    />
 }
 export default UnderConstruction;
